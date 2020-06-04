fdaad5c284b9bb6a0e9e638d64a16c3334821a1e
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-06-03 21:42:43 -0500

    Update Trigonometry.cpp

--- Trigonometric_Functions/Homework/Trigonometry.cpp
+++ Trigonometric_Functions/Homework/Trigonometry.cpp
@@ -1,6 +1,6 @@
 /***********************************************************************
  * Module:  Trigonometry.cpp
- * Author:  Muñoz Armas Francisco David & Daniela Orellana
+ * Author:  MuÃ±oz Armas Francisco David & Daniela Orellana
  * Modified: martes, 2 de junio de 2020 18:35:34
  * Purpose: Implementation of the class Trigonometry
  ***********************************************************************/
@@ -33,39 +33,37 @@ double Trigonometry::_sen(T base)
 ////////////////////////////////////////////////////////////////////////
 // Name:       Trigonometry::_cos(T base)
 // Purpose:    Implementation of Trigonometry::_cos()
-// Parameters:
+// Parameters: T base 
 // - base
 // Return:     double
 ////////////////////////////////////////////////////////////////////////
 
-template<typename T>
-double Trigonometry::_cos(T base)
-{
-	try {
-		if (base <= 0 || PI <= base) {
-			throw base;
+template <typename T>
+double Trigonometry::_cos(T base){
+	try{
+		if(base <= 0 || PI <= base){
+			throw base; 
 		}
 	}
-	catch (T e) {
+	catch (T e){
 		base = _check_angle(base);
 	}
 	return taylor._taylor_coseno(base, 10);
 }
-
 ////////////////////////////////////////////////////////////////////////
 // Name:       Trigonometry::_tg(T base)
 // Purpose:    Implementation of Trigonometry::_tg()
 // Parameters:
 // - base
 // Return:     double
-////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 
-template<typename T>
-double Trigonometry::_tg(T base)
-{
-	return _sen(base) / _cos(base);
+template <typename T>
+double Trigonometry::_tg(T base){
+	return _sen(base)/_cos(base);
 }
 
+	
 ////////////////////////////////////////////////////////////////////////
 // Name:       Trigonometry::_check_angle(T base)
 // Purpose:    Implementation of Trigonometry::_check_angle()
