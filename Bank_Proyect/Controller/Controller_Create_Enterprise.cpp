527f802587e39da4746c06178499e4af2901e5ca
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-06-26 22:58:17 -0500

    Update Controller_Create_Enterprise.cpp

--- Bank_Proyect/Controller/Controller_Create_Enterprise.cpp
+++ Bank_Proyect/Controller/Controller_Create_Enterprise.cpp
@@ -1,10 +1,11 @@
-/***********************************************************************
- * Module:  Array.cpp
- * Author:  USUARIO
- * Modified: jueves, 28 de mayo de 2020 19:07:11
- * Purpose: Implementation of the class Array
- ***********************************************************************/
 
+/*
+ * Universidad la Fuerzas Armadas ESPE
+ *
+ * @autor David Munoz & Daniela Orellana
+ * @date Jueves, 28 de mayo de 2020 19:07:11
+ * @function Implementation of Controller_Create_Enterprise
+*/
 #include "Controller_Create_Enterprise.h"
 
 #include <iostream>
@@ -18,20 +19,36 @@
 
 using namespace std;
 
+/**
+ * @brief _method
+ * @param 
+*/
 void Controller_Create_Enterprise::_method() {
 	_account_options();
 }
 
+/**
+ * @brief _account_options
+ * @param 
+*/
 void Controller_Create_Enterprise::_account_options() {
 	Bank_account _new_account(Account_type::_CHECKING_ACCOUNT, _set_data(), _set_balance(Account_type::_CHECKING_ACCOUNT));
 	cout << _new_account<< endl;
 	_fr._write_in_file(file,_path_account, &_new_account);
 }
 
+/**
+ * @brief _account_type_option
+ * @param
+*/
 int Controller_Create_Enterprise::_account_type_option() {
 	return 0;
 }
 
+/**
+ * @brief _set_data
+ * @param 
+*/
 Person Controller_Create_Enterprise::_set_data() {
 	char* _name;
 	char* _id;
@@ -52,6 +69,10 @@ Person Controller_Create_Enterprise::_set_data() {
 	return person;
 }
 
+/**
+ * @brief _set_person
+ * @param 
+*/
 char* Controller_Create_Enterprise::_set_person() {
 	char* _name;
 
