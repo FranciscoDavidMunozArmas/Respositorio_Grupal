f8e2f5f4055deeb886b52e5992f0611f21c547c3
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-07-01 15:07:36 -0500

    Update enterprise.cpp

--- Bank_Proyect/Proyect/enterprise.cpp
+++ Bank_Proyect/Proyect/enterprise.cpp
@@ -1,10 +1,10 @@
-/***********************************************************************
- * Module:  Enterprise.cpp
- * Author:  USUARIO
- * Modified: jueves, 4 de junio de 2020 20:33:40
- * Purpose: Implementation of the class Enterprise
- ***********************************************************************/
-
+/*
+ * Universidad la Fuerzas Armadas ESPE
+ *
+ * @autor David Munoz & Daniela Orellana
+ * @date Jueves, 4 de Junio de 2020 10:07:14
+ * @function Implementation of Controller
+*/
 #include "Enterprise.h"
 #include "Person.h"
 #include <iostream>
@@ -12,19 +12,25 @@
 
 using namespace std;
 
+/**
+ * @brief get_manager
+ * @param  
+*/
 char* Enterprise::get_manager() {
 	return _manager;
 }
 
+/**
+ * @brief set_manager
+ * @param _manager
+*/
 void Enterprise::set_manager(char* _manager)
 {
 	strcpy(this->_manager, _manager);
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       Enterprise::~Enterprise()
-// Purpose:    Implementation of Enterprise::~Enterprise()
-// Return:
-////////////////////////////////////////////////////////////////////////
-
+/**
+ * @brief Enterprise
+ * @param  
+*/
 Enterprise::~Enterprise(){}
