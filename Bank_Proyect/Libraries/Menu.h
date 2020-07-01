4c56da8f9bbc41d11e7c9fc815e139be6abb241e
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-07-01 00:41:42 -0500

    Update Menu.h

--- Bank_Proyect/Libraries/Menu.h
+++ Bank_Proyect/Libraries/Menu.h
@@ -1,10 +1,10 @@
-/***********************************************************************
- * Module:  Menu.h
- * Author:  USUARIO
- * Modified: jueves, 28 de mayo de 2020 19:07:11
- * Purpose: Declaration of the class Menu
- ***********************************************************************/
-
+/*
+ * Universidad la Fuerzas Armadas ESPE
+ *
+ * @autor David Munoz & Daniela Orellana
+ * @date Jueves, 28 de mayo de 2020 19:07:11
+ * @function Declaration of the class Menu
+*/
 #if !defined(__Menu)
 #define __Menu
 
