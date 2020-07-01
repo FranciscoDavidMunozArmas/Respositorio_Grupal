df212d7666401b575d9d66ac7827e10e15ef41e9
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-07-01 00:37:44 -0500

    Update to_number.cpp

--- Bank_Proyect/Libraries/to_number.cpp
+++ Bank_Proyect/Libraries/to_number.cpp
@@ -1,9 +1,11 @@
-/***********************************************************************
- * Module:  To_number.h
- * Author:  USUARIO
- * Modified: miércoles, 27 de mayo de 2020 15:07:22
- * Purpose: Declaration of the class ID_verificator
- ***********************************************************************/
+/*
+ * Universidad la Fuerzas Armadas ESPE
+ *
+ * @autor David Munoz & Daniela Orellana
+ * @date Jueves, 28 de mayo de 2020 10:07:14
+ * @function Implementation of to_number
+*/
+
 #define _GLIBCXX_USE_CXX11_ABI 0
 
 #include "To_number.h"
@@ -14,79 +16,69 @@
 
 using namespace std;
 
-////////////////////////////////////////////////////////////////////////
-// Name:       to_int(char* num)
-// Purpose:    Implementation of to_int(char* num)
-// Parameters:
-// - num
-// Return:     int
-////////////////////////////////////////////////////////////////////////
+/**
+ * @brief to_int
+ * @param  num
+*/
+
 int to_int(char* num) {
 	return atoi(num);
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       to_int(char num)
-// Purpose:    Implementation of to_int(char num)
-// Parameters:
-// - num
-// Return:     int
-////////////////////////////////////////////////////////////////////////
+/**
+ * @brief to_int
+ * @param  num
+*/
+
 int to_int(char num) {
 	return num - '0';
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       to_int(string num)
-// Purpose:    Implementation of to_int(string num)
-// Parameters:
-// - num
-// Return:     int
-////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief to_int
+ * @param  num
+*/
+
 int to_int(string num) {
 	return stoi(num);
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       to_float(char* num)
-// Purpose:    Implementation of to_float(char* num)
-// Parameters:
-// - num
-// Return:     float
-////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief to_float
+ * @param  num
+*/
+
 float to_float(char* num) {
 	return atof(num);
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       to_float(string num)
-// Purpose:    Implementation of to_float(string num)
-// Parameters:
-// - num
-// Return:     float
-////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief to_float
+ * @param  num
+*/
+
 float to_float(string num) {
 	return stof(num);
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       to_double(char* num)
-// Purpose:    Implementation of to_double(char* num)
-// Parameters:
-// - num
-// Return:     double
-////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief to_double
+ * @param  num
+*/
+
 double to_double(char* num) {
 	return atol(num);
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       to_double(string num)
-// Purpose:    Implementation of to_double(string num)
-// Parameters:
-// - num
-// Return:     double
-////////////////////////////////////////////////////////////////////////
+/**
+ * @brief to_double
+ * @param  num
+*/
+
 double to_double(std::string num) {
 	return stod(num);
 }
