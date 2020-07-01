9c1704cf7aecd81657c3eeb363080611deeb73d5
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-07-01 00:44:38 -0500

    Update main.cpp

--- Bank_Proyect/Main/main.cpp
+++ Bank_Proyect/Main/main.cpp
@@ -1,6 +1,11 @@
-
+/*
+ * Universidad la Fuerzas Armadas ESPE
+ *
+ * @autor David Munoz & Daniela Orellana
+ * @date Jueves, 28 de mayo de 2020 19:07:11
+ * @function Declaration of the class Main
+*/
 #include <iostream>
-
 #include "../Controller/Controller.cpp"
 #include "../Controller/Controller_Parent.cpp"
 
