0017018e502f894412973a91bc2f991e18327906
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-06-30 22:59:25 -0500

    Update Controller_Record.h

--- Bank_Proyect/Controller/Controller_Record.h
+++ Bank_Proyect/Controller/Controller_Record.h
@@ -1,10 +1,10 @@
-/***********************************************************************
- * Module:  Menu.h
- * Author:  USUARIO
- * Modified: jueves, 28 de mayo de 2020 19:07:11
- * Purpose: Declaration of the class Menu
- ***********************************************************************/
-
+/*
+ * Universidad la Fuerzas Armadas ESPE
+ *
+ * @autor David Munoz & Daniela Orellana
+ * @date Jueves, 28 de mayo de 2020 10:07:14
+ * @function Implementation of Controller_Record
+*/
 #if !defined(__Controller_Record)
 #define __Controller_Record
 
