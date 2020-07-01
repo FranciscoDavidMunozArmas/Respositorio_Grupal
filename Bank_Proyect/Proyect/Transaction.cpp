74932fda153b860fbbc27c8a41ba5a1fd7a410ff
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-07-01 15:01:32 -0500

    Update Transaction.cpp

--- Bank_Proyect/Proyect/Transaction.cpp
+++ Bank_Proyect/Proyect/Transaction.cpp
@@ -1,10 +1,10 @@
-/***********************************************************************
- * Module:  Transaction.cpp
- * Author:  David Muñoz & Daniela Orellana
- * Modified: martes, 2 de junio de 2020 15:34:44
- * Purpose: Implementation of the class Transaction
- ***********************************************************************/
-
+/*
+ * Universidad la Fuerzas Armadas ESPE
+ *
+ * @autor David Munoz & Daniela Orellana
+ * @date Martes, 2 de Junio de 2020 10:07:14
+ * @function Implementation of Transaction
+*/
 #include "Date.h"
 #include "Transaction.h"
 #include "Bank_account.h"
@@ -13,36 +13,37 @@
 
 #pragma once
 
-////////////////////////////////////////////////////////////////////////
-// Name:       Transaction::get_amount()
-// Purpose:    Implementation of Transaction::get_amount()
-// Return:     double
-////////////////////////////////////////////////////////////////////////
-
+/**
+ * @brief get_amount
+ * @param  
+*/
 double Transaction::get_amount()
 {
    return _amount;
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       Transaction::set_amount(const double _amount)
-// Purpose:    Implementation of Transaction::set_amount()
-// Parameters:
-// - new_amount
-// Return:     void
-////////////////////////////////////////////////////////////////////////
-
+/**
+ * @brief set_amount
+ * @param _amount
+*/
 void Transaction::set_amount(const double _amount)
 {
    this->_amount = _amount;
 }
 
-
+/**
+ * @brief bank_method
+ * @param _account
+*/
 double Transaction::bank_method(const Bank_account& _account)
 {
 	return 0;
 }
 
+/**
+ * @brief print_transaction
+ * @param  
+*/
 void Transaction::print_transaction() {
 	if (_type) {
 		cout << "Deposito: $";
@@ -54,12 +55,10 @@ void Transaction::print_transaction() {
 	cout << _amount << "\t\t" << _transaction_date.to_string() << endl;
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       Transaction::~Transaction()
-// Purpose:    Implementation of Transaction::~Transaction()
-// Return:
-////////////////////////////////////////////////////////////////////////
-
+/**
+ * @brief Transaction
+ * @param  
+*/
 Transaction::~Transaction()
 {
    // TODO : implement
