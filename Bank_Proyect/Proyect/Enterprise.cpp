8e3bc138076149de8b207e6f9d07e05b3c50edc0
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-07-01 14:55:35 -0500

    Update Enterprise.cpp

--- Bank_Proyect/Proyect/Enterprise.cpp
+++ Bank_Proyect/Proyect/Enterprise.cpp
@@ -1,10 +1,10 @@
-/***********************************************************************
- * Module:  Enterprise.cpp
- * Author:  USUARIO
- * Modified: jueves, 4 de junio de 2020 20:33:40
- * Purpose: Implementation of the class Enterprise
- ***********************************************************************/
-
+/*
+ * Universidad la Fuerzas Armadas ESPE
+ *
+ * @autor David Munoz & Daniela Orellana
+ * @date Jueves, 4 de Junio de 2020 10:07:14
+ * @function Implementation of Enterprise
+*/
 #include "Enterprise.h"
 #include "Natural_person.h"
 #include <iostream>
@@ -12,31 +12,34 @@
 
 using namespace std;
 
+/**
+ * @brief get_manager
+ * @param  
+*/
 Natural_person Enterprise::get_manager() {
 	return _manager;
 }
 
+/**
+ * @brief set_manager
+ * @param  _manager
+*/
 void Enterprise::set_manager(const Natural_person& _manager)
 {
 	this->_manager = _manager;
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       Enterprise::to_string()
-// Purpose:    Implementation of Enterprise::to_string()
-// Return:     string
-////////////////////////////////////////////////////////////////////////
-
+/**
+ * @brief to_string
+ * @param  
+*/
 string Enterprise::to_string() {
 	ostringstream oss;
 	oss << "Id:" << _id << "\tEmpresa:" << _name << "\n\tRepresentante legal: \n\tNombre:" << _manager.get_name();
 	return oss.str();
 }
-
-////////////////////////////////////////////////////////////////////////
-// Name:       Enterprise::~Enterprise()
-// Purpose:    Implementation of Enterprise::~Enterprise()
-// Return:
-////////////////////////////////////////////////////////////////////////
-
+/**
+ * @brief Enterprise
+ * @param  
+*/
 Enterprise::~Enterprise(){}
