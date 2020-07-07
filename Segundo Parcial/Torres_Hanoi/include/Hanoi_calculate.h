ce21253b3cd246e33e5e046a4823c40ec42dad28
Daniela Orellana <63482841+DanielaOrellana2000@users.noreply.github.com>
2020-07-06 19:53:49 -0500

    Add files via upload

---
+++ Segundo Parcial/Torres_Hanoi/include/Hanoi_calculate.h
@@ -0,0 +1,23 @@
+/******************************************************************
+*            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE              *
+*		              TORRES DE HANOI							  *
+* CARRERA: Ingenieria de Software                                 *
+* NOMBRE: Daniela Orellana y David Muñoz                          *
+* FECHA DE CREACIÓN: 04/07/2020                                   *
+* NRC: 6396                                                       *
+*******************************************************************/
+#ifndef HANOI_CALCULATE_H
+#define HANOI_CALCULATE_H
+
+class Hanoi_calculate
+{
+    public:
+        Hanoi_calculate();
+        int _calculate_movements(int);
+
+    protected:
+
+    private:
+};
+
+#endif // HANOI_CALCULATE_H
